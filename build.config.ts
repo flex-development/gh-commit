b3c8007a999eb61f86c3c7fdf42f1aaf58968922
Lexus Drumgold <unicornware@flexdevelopment.llc>
2023-11-04 03:26:22 -0400

    build: tree shake `package.json` imports
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- build.config.ts
+++ build.config.ts
@@ -4,7 +4,9 @@
  */
 
 import { defineBuildConfig, type Config } from '@flex-development/mkbuild'
-import { at } from '@flex-development/tutils'
+import pathe from '@flex-development/pathe'
+import { at, pick } from '@flex-development/tutils'
+import type * as esbuild from 'esbuild'
 import pkg from './package.json' assert { type: 'json' }
 import tsconfig from './tsconfig.build.json' assert { type: 'json' }
 
@@ -26,6 +28,33 @@ const config: Config = defineBuildConfig({
     'class-validator'
   ],
   platform: 'node',
+  plugins: [
+    {
+      name: 'pkg',
+
+      /**
+       * Tree shake `package.json` imports.
+       *
+       * [1]: https://esbuild.github.io/plugins
+       *
+       * @param {esbuild.PluginBuild} build - [esbuild plugin api][1]
+       * @param {esbuild.PluginBuild['onLoad']} build.onLoad - Load callback
+       * @return {void} Nothing when complete
+       */
+      setup({ onLoad }: esbuild.PluginBuild): void {
+        return void onLoad({ filter: /package\.json$/ }, args => {
+          if (args.path === pathe.resolve('package.json')) {
+            return {
+              contents: JSON.stringify(pick(pkg, ['name', 'version']), null, 2),
+              loader: 'json'
+            }
+          }
+
+          return null
+        })
+      }
+    }
+  ],
   source: 'src/main.ts',
   sourcemap: true,
   sourcesContent: false,
