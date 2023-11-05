caa94b1b7c54b9f09b1b42871b2a368085fc3b57
Lexus Drumgold <unicornware@flexdevelopment.llc>
2023-11-05 04:15:15 -0500

    ci(workflows): add `release-chore`
    - replaces `release-branch`
    
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,24 +6,14 @@ set -e
 #
 # 1. run typecheck
 # 2. run tests
-# 3. pack project
-# 4. run postbuild typecheck
-# 5. get release version data
-# 6. get release branch name
-# 7. create release branch
-# 8. push release branch
-# 9. cleanup
+# 3. create release chore commit
 #
 # References:
 #
-# - https://github.com/arethetypeswrong/arethetypeswrong.github.io
+# - https://git-scm.com/docs/git-commit
+# - https://github.com/flex-development/grease
+# - https://jqlang.github.io
 
 yarn typecheck
 yarn test:cov
-yarn pack
-yarn check:types:build
-RELEASE_VERSION=$(grease bump -j $@)
-RELEASE_BRANCH=release/$(jq .version -r <<<$RELEASE_VERSION)
-git branch $RELEASE_BRANCH
-git push origin --no-verify --set-upstream $RELEASE_BRANCH
-yarn clean:pack
+git commit --allow-empty -S -s -m "release(chore): $(jq .version -r <<<$(grease bump -j $@))"
